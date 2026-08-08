15ecf326ec42a02490c2cccfa9f297fffbddf3e1
playwithrw <okxataspin1@gmail.com>
2026-08-08 09:06:10 -0400

    fix(web): drop duplicate story editor export

--- lib/stories/stories.dart
+++ lib/stories/stories.dart
@@ -2,6 +2,5 @@ export 'bloc/create_stories_bloc.dart';
 export 'bloc/stories_bloc.dart';
 export 'bloc/user_stories_bloc.dart';
 export 'editor/story_editor_platform.dart';
-export 'editor/story_editor_web.dart';
 export 'view/stories_page.dart';
 export 'widgets/widgets.dart';
